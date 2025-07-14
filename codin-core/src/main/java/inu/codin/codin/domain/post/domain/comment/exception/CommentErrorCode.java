10fd61177379059fe745b75c3edad187252c7d80
gimgisu <gisu1102@gmail.com>
2025-07-15 00:04:26 +0900

    refactor: id 형변환ObjectUtil 사용 및 에러코드 수정

--- codin-core/src/main/java/inu/codin/codin/domain/post/domain/comment/exception/CommentErrorCode.java
+++ codin-core/src/main/java/inu/codin/codin/domain/post/domain/comment/exception/CommentErrorCode.java
@@ -6,8 +6,7 @@ import org.springframework.http.HttpStatus;
 
 @RequiredArgsConstructor
 public enum CommentErrorCode implements GlobalErrorCode {
-    COMMENT_NOT_FOUND(HttpStatus.NOT_FOUND, "댓글을 찾을 수 없습니다."),
-    POST_NOT_FOUND(HttpStatus.NOT_FOUND, "게시물을 찾을 수 없습니다.");
+    COMMENT_NOT_FOUND(HttpStatus.NOT_FOUND, "댓글을 찾을 수 없습니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
