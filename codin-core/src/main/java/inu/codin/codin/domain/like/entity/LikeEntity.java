56a3f956d4e96e71b4aaac84b2147381bdb80400
X1n9fU <minkyeong0244@gmail.com>
2025-07-24 02:08:04 +0900

    refactor : LikeType에 Lectures를 추가함에 따라 likeTypeId 를 ObjectId에서 String으로 변경

--- codin-core/src/main/java/inu/codin/codin/domain/like/entity/LikeEntity.java
+++ codin-core/src/main/java/inu/codin/codin/domain/like/entity/LikeEntity.java
@@ -12,12 +12,12 @@ import org.springframework.data.mongodb.core.mapping.Document;
 public class LikeEntity extends BaseTimeEntity {
     @Id
     private ObjectId _id;
-    private ObjectId likeTypeId; // 게시글, 댓글, 대댓글의 ID
+    private String likeTypeId; // 게시글, 댓글, 대댓글의 ID
     private LikeType likeType; // 엔티티 타입 (post, comment, reply)
     private ObjectId userId; // 좋아요를 누른 사용자 ID
 
     @Builder
-    public LikeEntity(ObjectId likeTypeId, LikeType likeType, ObjectId userId) {
+    public LikeEntity(String likeTypeId, LikeType likeType, ObjectId userId) {
         this.likeTypeId = likeTypeId;
         this.likeType = likeType;
         this.userId = userId;
--- codin-core/src/main/java/inu/codin/codin/domain/post/domain/comment/service/CommentService.java
+++ codin-core/src/main/java/inu/codin/codin/domain/post/domain/comment/service/CommentService.java
@@ -123,7 +123,7 @@ public class CommentService {
                     }
                     return CommentResponseDTO.commentOf(comment, nickname, userImageUrl,
                             replyCommentService.getRepliesByCommentId(post.getAnonymous(), comment.get_id()),
-                            likeService.getLikeCount(LikeType.COMMENT, comment.get_id()),
+                            likeService.getLikeCount(LikeType.COMMENT, comment.get_id().toString()),
                             getUserInfoAboutComment(comment.get_id()));
                 })
                 .toList();
@@ -146,7 +146,7 @@ public class CommentService {
     public UserInfo getUserInfoAboutComment(ObjectId commentId) {
         ObjectId userId = SecurityUtils.getCurrentUserId();
         return UserInfo.builder()
-                .isLike(likeService.isLiked(LikeType.COMMENT, commentId, userId))
+                .isLike(likeService.isLiked(LikeType.COMMENT, commentId.toString(), userId))
                 .build();
     }
 
