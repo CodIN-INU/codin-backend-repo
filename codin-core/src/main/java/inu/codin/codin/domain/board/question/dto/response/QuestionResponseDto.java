d0cb6255b04221a2412b759b237acf2bd46788a8
X1n9fU <minkyeong0244@gmail.com>
2025-08-19 23:28:11 +0900

    feat : Response 필드 값에 id 추가

--- codin-core/src/main/java/inu/codin/codin/domain/board/question/dto/response/QuestionResponseDto.java
+++ codin-core/src/main/java/inu/codin/codin/domain/board/question/dto/response/QuestionResponseDto.java
@@ -8,11 +8,13 @@ import lombok.Getter;
 @AllArgsConstructor
 public class QuestionResponseDto {
 
+    private String id;
     private String question;
     private String answer;
 
     public static QuestionResponseDto of(QuestionEntity questionEntity) {
         return new QuestionResponseDto(
+                questionEntity.get_id().toString(),
                 questionEntity.getQuestion(),
                 questionEntity.getAnswer()
         );
