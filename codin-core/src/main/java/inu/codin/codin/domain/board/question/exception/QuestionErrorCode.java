7fc91d70967a324fae84ace9f800764ad7401e48
X1n9fU <minkyeong0244@gmail.com>
2025-08-19 23:24:16 +0900

    feat : 자주 묻는 질문(Question) CRUD 구현 완료

---
+++ codin-core/src/main/java/inu/codin/codin/domain/board/question/exception/QuestionErrorCode.java
@@ -0,0 +1,23 @@
+package inu.codin.codin.domain.board.question.exception;
+
+import inu.codin.codin.common.exception.GlobalErrorCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@RequiredArgsConstructor
+public enum QuestionErrorCode implements GlobalErrorCode {
+    QUESTION_NOT_FOUND(HttpStatus.NOT_FOUND, "질문을 찾을 수 없습니다.");
+
+    private final HttpStatus httpStatus;
+    private final String message;
+
+    @Override
+    public HttpStatus httpStatus() {
+        return httpStatus;
+    }
+
+    @Override
+    public String message() {
+        return message;
+    }
+}
