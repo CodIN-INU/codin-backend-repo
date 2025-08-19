2bc2fac67fff7bc06ff1ccb11421b4d5b22f7efd
X1n9fU <minkyeong0244@gmail.com>
2025-08-19 23:16:07 +0900

    feat : GlobalExceptionHandler에 Notice, Question Exception 추가

--- codin-core/src/main/java/inu/codin/codin/domain/board/notice/exception/NoticeException.java
+++ codin-core/src/main/java/inu/codin/codin/domain/board/notice/exception/NoticeException.java
@@ -1,10 +1,13 @@
 package inu.codin.codin.domain.board.notice.exception;
 
 import inu.codin.codin.common.exception.GlobalException;
+import lombok.Getter;
 
+@Getter
 public class NoticeException extends GlobalException {
-
+    private final NoticeErrorCode errorCode;
     public NoticeException(NoticeErrorCode errorCode) {
         super(errorCode);
+        this.errorCode = errorCode;
     }
 }
