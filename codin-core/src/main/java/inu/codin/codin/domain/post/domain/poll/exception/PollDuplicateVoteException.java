b5c0efae210d9030ec369403e446f4715785e9ce
gimgisu <gisu1102@gmail.com>
2025-08-20 17:39:52 +0900

    refactor: 예외 처리 구조를 ErrorCode + PollException 기반으로 개선 및 도메인별 책임 분리

--- codin-core/src/main/java/inu/codin/codin/domain/post/domain/poll/exception/PollDuplicateVoteException.java
+++
@@ -1,7 +0,0 @@
-package inu.codin.codin.domain.post.domain.poll.exception;
-
-public class PollDuplicateVoteException extends RuntimeException {
-    public PollDuplicateVoteException(String message) {
-        super(message);
-    }
-}
