892dce3dd93e58f36de689a6774ff140090d0bb6
Kwak Byoung Min <rovin1273@gmail.com>
2025-09-08 12:15:59 +0900

    fix: fcm 토큰 저장 로직 수정
    - Swagger 명세를 인터페이스로 분리
    - Swagger 명세 예시 및 이름 추가

--- codin-core/src/main/java/inu/codin/codin/infra/fcm/dto/request/FcmTokenRequest.java
+++ codin-core/src/main/java/inu/codin/codin/infra/fcm/dto/request/FcmTokenRequest.java
@@ -9,11 +9,11 @@ import lombok.Getter;
 @Getter
 public class FcmTokenRequest {
 
-    @Schema(description = "Fcm Token")
+    @Schema(description = "Fcm Token", example = "FCM 토큰")
     @NotBlank
     private String fcmToken;
 
-    @Schema(description = "Android, IOS")
+    @Schema(description = "Android, IOS", example = "디바이스 종류")
     @NotBlank
     private String deviceType;
 
