0bc9ec76eead836759c547c2ada462f3ab74984f
gisu1102 <76506573+gisu1102@users.noreply.github.com>
2025-10-08 19:49:28 +0900

    Revert "fix: post 단건/목록 JSON을 현재 프론트 요구사항에 맞게 정규화"

--- codin-core/src/main/java/inu/codin/codin/domain/post/dto/response/PostPageItemResponseDTO.java
+++ codin-core/src/main/java/inu/codin/codin/domain/post/dto/response/PostPageItemResponseDTO.java
@@ -1,14 +1,10 @@
 package inu.codin.codin.domain.post.dto.response;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonUnwrapped;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
-@JsonInclude(JsonInclude.Include.NON_NULL)
 public class PostPageItemResponseDTO {
-    @JsonUnwrapped // 추후 프론트에 얘기해서 {post:{}, poll:{} 구조로 변경} 현재:{,,,poll:{}}
     private final PostDetailResponseDTO post;
     private final PollInfoResponseDTO poll;
 
