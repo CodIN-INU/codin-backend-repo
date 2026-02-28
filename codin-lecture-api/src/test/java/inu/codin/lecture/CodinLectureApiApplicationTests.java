91fcbc2b91d851233f439d6a1ed0b27a234642ca
d6nggyun <d6nggyun@gmail.com>
2026-02-28 19:39:27 +0900

    fix: TestApp에 @EnableAutoConfiguration 추가

--- codin-lecture-api/src/test/java/inu/codin/lecture/CodinLectureApiApplicationTests.java
+++ codin-lecture-api/src/test/java/inu/codin/lecture/CodinLectureApiApplicationTests.java
@@ -4,22 +4,30 @@ import co.elastic.clients.elasticsearch.ElasticsearchAsyncClient;
 import co.elastic.clients.elasticsearch.ElasticsearchClient;
 import co.elastic.clients.elasticsearch._helpers.bulk.BulkIngester;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
-@SpringBootApplication(scanBasePackages = {
-        "inu.codin.codin.domain.admin",
-        "inu.codin.codin.domain.elasticsearch",
-        "inu.codin.codin.domain.lecture",
-        "inu.codin.codin.domain.like",
-        "inu.codin.codin.domain.review",
-        "inu.codin.codin.global",
-        "inu.codin.codin.domain.user.service"
-})
 @ActiveProfiles("test")
+@SpringBootTest(classes = CodinLectureApiApplicationTests.TestApp.class)
 class CodinLectureApiApplicationTests {
+
+    @SpringBootConfiguration
+    @EnableAutoConfiguration
+    @ComponentScan(basePackages = {
+            "inu.codin.codin.domain.admin",
+            "inu.codin.codin.domain.elasticsearch",
+            "inu.codin.codin.domain.lecture",
+            "inu.codin.codin.domain.like",
+            "inu.codin.codin.domain.review",
+            "inu.codin.codin.global",
+            "inu.codin.codin.domain.user.service"
+    })
+    static class TestApp { }
 
     @MockitoBean
     private RedisTemplate<String, Object> redisTemplate;
