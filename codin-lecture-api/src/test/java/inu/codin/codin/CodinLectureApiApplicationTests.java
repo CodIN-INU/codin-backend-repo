a0fd11a4583ba88dc6dd1e7099ddccff8c77fa49
d6nggyun <d6nggyun@gmail.com>
2026-02-28 18:57:30 +0900

    fix: CodinLectureApiApplicationTests 테스트 어플리케이션 지정

--- codin-lecture-api/src/test/java/inu/codin/codin/CodinLectureApiApplicationTests.java
+++ codin-lecture-api/src/test/java/inu/codin/codin/CodinLectureApiApplicationTests.java
@@ -4,12 +4,19 @@ import co.elastic.clients.elasticsearch.ElasticsearchAsyncClient;
 import co.elastic.clients.elasticsearch.ElasticsearchClient;
 import co.elastic.clients.elasticsearch._helpers.bulk.BulkIngester;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
-@SpringBootTest(classes = CodinLectureApiApplication.class)
+@SpringBootApplication(scanBasePackages = {
+        "inu.codin.codin.domain.admin",
+        "inu.codin.codin.domain.elasticsearch",
+        "inu.codin.codin.domain.lecture",
+        "inu.codin.codin.domain.like",
+        "inu.codin.codin.domain.review",
+        "inu.codin.codin.global"
+})
 @ActiveProfiles("test")
 class CodinLectureApiApplicationTests {
 
