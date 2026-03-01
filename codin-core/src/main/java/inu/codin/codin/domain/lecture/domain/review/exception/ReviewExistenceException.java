b0e8602cb1528ac63be8ca953cc53318efae0aca
gimgisu <gisu1102@gmail.com>
2026-03-01 15:42:36 +0900

    refactor(core): lecture-api로 완전 이전된 레거시 lecture 패키지 삭제
    SyncScheduler에서 ReviewEntity/ReviewRepository/ReviewService 의존과
    recoverReviews() 메서드를 제거하고, domain/lecture/ 패키지(28파일)를 삭제한다.
    LikeType.LECTURE/REVIEW는 Feign 연동에 필요하므로 유지한다.

--- codin-core/src/main/java/inu/codin/codin/domain/lecture/domain/review/exception/ReviewExistenceException.java
+++
@@ -1,7 +0,0 @@
-package inu.codin.codin.domain.lecture.domain.review.exception;
-
-public class ReviewExistenceException extends RuntimeException{
-    public ReviewExistenceException(String message){
-        super(message);
-    }
-}
