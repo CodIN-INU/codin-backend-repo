c9d5f5008b88f02038691eb5f64b906cd7d28bef
d6nggyun <d6nggyun@gmail.com>
2026-03-05 13:03:18 +0900

    feat: 유저의 전체적인 정보를 수정하는 api 추가

---
+++ codin-core/src/main/java/inu/codin/codin/domain/user/dto/request/UpdateUserInfoRequestDto.java
@@ -0,0 +1,21 @@
+package inu.codin.codin.domain.user.dto.request;
+
+import inu.codin.common.entity.College;
+import inu.codin.common.entity.Department;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+public record UpdateUserInfoRequestDto(
+
+        @Schema(description = "닉네임", example = "코딩")
+        String nickname,
+
+        @Schema(description = "이름", example = "김철수")
+        String name,
+
+        @Schema(description = "단과대", example = "INFORMATION_TECHNOLOGY")
+        College college,
+
+        @Schema(description = "학과", example = "COMPUTER_SCI")
+        Department department
+) {
+}
