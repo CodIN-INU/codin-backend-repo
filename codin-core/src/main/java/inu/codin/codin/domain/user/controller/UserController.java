ddfc41a376fd5bfb887feac2ab5288b01b05ea55
d6nggyun <d6nggyun@gmail.com>
2026-03-05 13:14:58 +0900

    fix: 유저 전체적 정보 수정 endpoint 수정

--- codin-core/src/main/java/inu/codin/codin/domain/user/controller/UserController.java
+++ codin-core/src/main/java/inu/codin/codin/domain/user/controller/UserController.java
@@ -98,7 +98,7 @@ public class UserController {
     @Operation(
             summary = "유저 전체적인 정보 수정"
     )
-    @PutMapping("/nickname-name")
+    @PutMapping("/update/user-info")
     public ResponseEntity<SingleResponse<?>> updateUserInfo(@RequestBody @Valid UpdateUserInfoRequestDto updateUserInfoRequestDto){
         userService.updateUserInfo(updateUserInfoRequestDto);
         return ResponseEntity.ok()
