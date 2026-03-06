cb77207079adb99f42388cfda9280c7e2c6aaa1d
RYUJEONGHUN <ryugh0210@naver.com>
2026-03-07 03:00:17 +0900

    @Valid 추가

--- codin-core/src/main/java/inu/codin/codin/domain/calendar/controller/CalendarControllerImpl.java
+++ codin-core/src/main/java/inu/codin/codin/domain/calendar/controller/CalendarControllerImpl.java
@@ -38,7 +38,7 @@ public class CalendarControllerImpl implements CalendarController {
 
     @PostMapping("/events")
     @PreAuthorize("hasAnyRole('MANAGER', 'ADMIN')")
-    public ResponseEntity<SingleResponse<CalendarCreateResponse>> create(@RequestBody CalendarCreateRequest request) {
+    public ResponseEntity<SingleResponse<CalendarCreateResponse>> create(@Valid @RequestBody CalendarCreateRequest request) {
         return ResponseEntity.status(201).body(new SingleResponse<>(201, "켈린더 이벤트 생성 완료",
                 calendarService.create(request)));
     }
